b669bcae526e74b2f8d3b3ce802a4c5ec0655d09
Suchit-Aryal <144269180+Suchit-Aryal@users.noreply.github.com>
2025-11-13 17:02:29 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,119 @@
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import StringField, SubmitField,SelectField
+from wtforms.fields.simple import URLField
+from wtforms.validators import DataRequired, Regexp, URL
+import csv
+import sklearn
+
+
+time_format = Regexp(
+    r'^[0-2][0-9]:[0-5][0-9]$',
+    message='Time must be in HH:MM format'
+)
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe name', validators=[DataRequired()])
+    location=StringField('Location URL',validators=[DataRequired(),URL()])
+    opening_time=StringField('Open Time',validators=[DataRequired(),time_format])
+    closing_time=StringField('Close Time',validators=[DataRequired(),time_format])
+    Coffee_rating=SelectField('Coffee Rating',choices=[
+        ('☕️', '☕️'),
+        ('☕️☕️', '☕️☕️'),
+        ('☕️☕️☕️', '☕️☕️☕️'),
+        ('☕️☕️☕️☕️', '☕️☕️☕️☕️'),
+        ('☕️☕️☕️☕️☕️', '☕️☕️☕️☕️☕️')
+    ], validators=[DataRequired()])
+
+    wifi_rating=SelectField('Wifi Rating',choices=[
+        ('✘','✘'),
+        ('💪', '💪'),
+        ('💪💪', '💪💪'),
+        ('💪💪💪', '💪💪💪'),
+        ('💪💪💪💪', '💪💪💪💪'),
+        ('💪💪💪💪💪', '💪💪💪💪💪')]
+        ,validators=[DataRequired()])
+
+    power_outlet_rating=SelectField('Power Outlet Rating',choices=
+            [
+            ('✘', '✘'),
+            ('🔌', '🔌'),
+            ('🔌🔌', '🔌🔌'),
+            ('🔌🔌🔌', '🔌🔌🔌'),
+            ('🔌🔌🔌🔌', '🔌🔌🔌🔌'),
+            ('🔌🔌🔌🔌🔌', '🔌🔌🔌🔌🔌')
+            ],validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# Exercise:
+# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
+# make coffee/wifi/power a select element with choice of 0 to 5.
+#e.g. You could use emojis ☕️/💪/✘/🔌
+# make all fields required except submit
+# use a validator to check that the URL field has a URL entered.
+# ---------------------------------------------------------------------------
+#------------FOR THE FORM THE INPUTS---------------#
+
+
+# all Flask routes below
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/add',methods=['POST','GET'])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        with open('./cafe-data.csv',mode='a',newline='',encoding='utf-8') as csv_file:
+            writer=csv.writer(csv_file)
+            writer.writerow(
+                [
+                    form.cafe.data,
+                    form.location.data,
+                    form.opening_time.data,
+                    form.closing_time.data,
+                    form.Coffee_rating.data,
+                    form.wifi_rating.data,
+                    form.power_outlet_rating.data
+                ]
+            )
+        return redirect(url_for('cafes'))
+    # Exercise:
+    # Make the form write a new row into cafe-data.csv
+    # with   if form.validate_on_submit()
+    return render_template('add.html', form=form)
+
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html', cafes=list_of_rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
